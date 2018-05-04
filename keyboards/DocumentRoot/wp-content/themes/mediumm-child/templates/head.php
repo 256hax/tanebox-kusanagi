c6ae2686f585a9aece1845823e7116c83cb8d3c7
256hax <256hax@GitHub>
2018-05-04 19:48:38 +0900

    style: add Google Japanese Fonts.

---
+++ keyboards/DocumentRoot/wp-content/themes/mediumm-child/templates/head.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jojoee\Mediumm\Templates\Head;
+
+?>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet" />
+  <?php wp_head(); ?>
+</head>
