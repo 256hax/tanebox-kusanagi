f0cfbc26e7ef81d0a386eff13e2375e3e9681d50
256hax <256hax@GitHub>
2018-05-06 09:26:03 +0900

    chore: add Google Analytics tag.

--- keyboards/DocumentRoot/wp-content/themes/mediumm-child/templates/head.php
+++ keyboards/DocumentRoot/wp-content/themes/mediumm-child/templates/head.php
@@ -8,6 +8,16 @@ namespace Jojoee\Mediumm\Templates\Head;
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118004633-3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-118004633-3');
+  </script>
+  <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet" />
   <?php wp_head(); ?>
 </head>
