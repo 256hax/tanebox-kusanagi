4a70140903fa26641908b62856b32084418b2458
256hax <256hax@GitHub>
2018-05-08 00:07:24 +0900

    docs: add copyright.

---
+++ keyboards/DocumentRoot/wp-content/themes/twentyseventeen-child/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'http://tanebox.com/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'by 256hax', 'twentyseventeen' ), 'WordPress' ); ?></a>
+</div><!-- .site-info -->
