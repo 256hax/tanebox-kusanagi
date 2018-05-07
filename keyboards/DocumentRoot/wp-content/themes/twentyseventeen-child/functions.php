5da07530393b33a5dead30b635c73dcbf51f32ec
256hax <256hax@GitHub>
2018-05-07 21:24:18 +0900

    style: change theme. and init commit for child theme.

---
+++ keyboards/DocumentRoot/wp-content/themes/twentyseventeen-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+// テーマの読込
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style' ) );
+}
+
+// Markdown Editor Plugin.
+// By default Markdown Editor is only enabled on Posts, but you can enable it on pages and custom post types by adding post type support.
+add_post_type_support( 'page', 'wpcom-markdown' );
+?>
